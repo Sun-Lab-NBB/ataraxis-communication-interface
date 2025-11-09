e562254201d35909a4bf30ba61b7026ddd99e906
Inkaros <ikondratv@gmail.com>
2025-11-09 16:25:44 -0500

    Removed leftower artifacts from testing 4.0.1 fixes

--- examples/example_runtime.py
+++ examples/example_runtime.py
@@ -11,8 +11,6 @@
 # API documentation: https://ataraxis-communication-interface-api.netlify.app/.
 # Authors: Ivan Kondratyev (Inkaros), Jacob Groner.
 
-import multiprocessing
-
 # Imports the necessary assets, including the TestModuleInterface class
 from pathlib import Path
 
